68a5c9b8e41d25ab0a7b5d8400e07a4363eedffc
ksavage97 <ksavage97@gmail.com>
2016-10-21 15:03:24 -0700

    Migration merge

---
+++ Puppr.API/Migrations/201610212202566_Merge.cs
@@ -0,0 +1,16 @@
+namespace Puppr.API.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Merge : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
